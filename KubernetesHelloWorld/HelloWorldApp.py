68cb0eb7d0490461c42115077ee0f8af98d01aa4
Roger Pease <rogerpease@gmail.com>
2022-10-03 18:52:46 -0700

    Initial Revision

---
+++ KubernetesHelloWorld/HelloWorldApp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3 
+
+import os
+
+# Run:
+#  wget -O - -q http://localhost:5000/   to see result.  
+
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def result():
+    return "Hello Roger!\n" # response to your request.
+
+if __name__ == "__main__":
+   os.system("ifconfig")  # Get IP address of this system 
+   app.run(debug=True,host="0.0.0.0")  # host=0.0.0.0 makes it pick all network interfaces not just loopback
