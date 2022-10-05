5c1f50daf35f340d7f59a2f086febb4a6efe6ea5
Roger Pease <rogerpease@gmail.com>
2022-10-05 11:44:28 -0700

    Initial Revision

--- KubernetesHelloWorld/HelloWorldApp.py
+++ KubernetesHelloWorld/HelloWorldApp.py
@@ -13,5 +13,5 @@ def result():
     return "Hello Roger!\n" # response to your request.
 
 if __name__ == "__main__":
-   os.system("ifconfig")  # Get IP address of this system 
-   app.run(debug=True,host="0.0.0.0")  # host=0.0.0.0 makes it pick all network interfaces not just loopback
+  os.system("ifconfig")  # Get IP address of this system 
+  app.run(debug=True,host="0.0.0.0",port=8080)  # host=0.0.0.0 makes it pick all network interfaces not just loopback
